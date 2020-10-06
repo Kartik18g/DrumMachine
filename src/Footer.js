c333f32450a037c80914896645c847a793efabdb
Kartik Gupta <gupta.kartik18kg@gmail.com>
2020-10-06 19:36:23 +0530

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="page-footer font-small">
+        <div className="footer-copyright text-center py-3 text-white">
+          © 2020
+          <a href="https://github.com/kartik18g" className="text-white">
+            {" "}
+            By Kartik Gupta{" "}
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
